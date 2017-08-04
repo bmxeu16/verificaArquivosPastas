0681277f5421ec18d868a4faf094430868df7b4a
wmeier <wmeier@192-168-56-1.privateip>
2017-08-04 15:45:11 -0300

    Criado classe VerificaPasta, essa é responsavel por capturar as informações dos arquivos que estão na pasta e comparar o tamanho do volume de dados que está presente.

--- verificaArquivosPastas/src/negocio/Aplicativo.java
+++ verificaArquivosPastas/src/negocio/Aplicativo.java
@@ -1,31 +1,14 @@
 package negocio;
 
-import java.io.File;
-
 public class Aplicativo {
 
 	public static void main(String[] args) {
 		
-		String nomeArquivo;
-		float tamanhoArquivo;
+		VerificaPasta pasta = new VerificaPasta();
+				
 		
-		File arquivo = new File("/home/william/Documentos"); 
-        File[] file = arquivo.listFiles(); 
-        if(file != null){ 
-            int length = file.length; 
-            for(int i = 0; i < length; ++i){ 
-                File f = file[i]; 
-                if(f.isFile()){ 
-                    nomeArquivo = f.getName();
-                	tamanhoArquivo = f.length();
-                    System.out.println(nomeArquivo + ", " + tamanhoArquivo); 
-                } 
-                else if(f.isDirectory()){ 
-                    System.out.println("Diretorio: " + f.getName()); 
-                } 
-            }
-        }
-
+		while(true){
+			pasta.comparaTamanhos();
+		}
 	}
-
 }
---
+++ verificaArquivosPastas/src/negocio/VerificaPasta.java
@@ -0,0 +1,69 @@
+package negocio;
+
+import java.io.File;
+
+public class VerificaPasta {
+
+	String nomeArquivo;
+	double tamanhoArquivos;
+	double tamanhoArquivos2;
+	
+	double converteTotal;
+	
+	
+	public double buscaArquivos(){		
+		
+		double tamanhoTotal = 0;
+		int contador = 0;
+		String arquivos[] = new String[10];
+		double tamanho[] = new double[10];
+		
+		File arquivo = new File("C:\\edi\\elavon\\destino\\outbox"); 
+        File[] file = arquivo.listFiles(); 
+        if(file != null){ 
+            int length = file.length; 
+            //System.out.println("Verifica os arquivos");
+            for(int i = 0; i < length; ++i){ 
+                File f = file[i]; 
+                if(f.isFile()){
+                	
+                	arquivos[contador] = f.getName();
+                	tamanho[contador] = f.length();
+                	tamanho[contador] = tamanho[contador] / 1000;
+                    //System.out.println(arquivos[contador] + ", " + tamanho[contador] + " KB");
+                	tamanhoTotal = tamanhoTotal + tamanho[contador];
+                	contador++;
+                } 
+                else if(f.isDirectory()){ 
+                    //System.out.println("Diretorio: " + f.getName()); 
+                } 
+            }
+            System.out.println("Total de Arquivos encontrado: " + contador);
+            converteTotal = tamanhoTotal / 1000;
+            System.out.println("Tamanho Total: " + converteTotal + " KB"); 
+        }
+        return tamanhoTotal;
+	}
+	
+	public void comparaTamanhos(){
+		
+		System.out.println("Verifica Tamanho!");
+		tamanhoArquivos = buscaArquivos();
+		
+		System.out.println("Delay 10000.");
+		try {
+			Thread.sleep(10000);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		tamanhoArquivos2 = buscaArquivos();
+		
+		if(tamanhoArquivos2 > tamanhoArquivos){
+			System.out.println("Não faz nada");
+		}else if(tamanhoArquivos2 == tamanhoArquivos){
+			System.out.println("Executa tarefa!!!");
+			//colocar delay de 5min depois que executar a tarefa!
+		}
+	}
+}
